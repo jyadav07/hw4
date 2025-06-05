1102723232b1741dab7fcce6614ccd65250170f2
Jaya Yadav <jaya.yadav@kellogg.northwestern.edu>
2025-06-05 20:04:24 +0000

    newchanges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
+    @user["username"] = params["username"]
     @user["email"] = params["email"]
 
     # encrypt user's password before storing
